1b5d02dd1a39a20f99638d22648d09355e13a1b2
RahmanFiros <rahmanfiros123@gmail.com>
2018-05-01 03:04:02 +0600

    Main Written!!!

--- src/takeout/TakeOut.java
+++ src/takeout/TakeOut.java
@@ -12,6 +12,28 @@ public class TakeOut {
 
     
     public static void main(String[] args) {
+        Students students = new Students();
+        students.s1="Chicken Patty";
+        students.s2="Chili Sauce";
+        students.s3="Ricotta Cheese";
+        
+        Students students2 = new Students();
+        students2.s1="Beef Patty";
+        students2.s2="Tomato Sauce";
+        students2.s3="Stilton Cheese";
+        
+        Waiter chickenmaker = new ChickenBurger();
+        Waiter beefmaker = new BeefBurger();
+        
+        // Making ChickenBurger
+        students.Construct(chickenmaker);
+        Burger chcknB = chickenmaker.getBurger();
+        chcknB.Show();
+        
+        //Making BeefBurger
+        students2.Construct(beefmaker);
+        Burger beefB = beefmaker.getBurger();
+        beefB.Show();
         
     }
     
